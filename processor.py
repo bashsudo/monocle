a6fa16be8a6dbb49f5c4813b25c2f38682706eb8
Eiza Stanford <bashsudormstar@gmail.com>
2022-05-12 12:57:46 -0700

    New RegionChunk class; new global "check" functions; more work on Region
    Global function imageBoundsCheck returns True if a coordinate (x, y) is
    within the bounds of the image; imageBoundsCheckRadius is very similar
    but takes a radius into account: the radius cannot extend out of the image.
    
    In the RegionChunk class, method chunkDefineExact creates a grid of Region
    objects by starting from a specific location/corner and creating a given
    number of Regions that are some pixels apart based on the "jump" parameters.
    
    In the Region class, it is now possible to fill in a Region object of a
    specific color with imageFillRegion.
    
    New test functions regionTestA and chunkTestA test the Region and RegionChunk
    class respectively by creating an object of each class and interacting with
    some of the important methods that have been written.

--- processor.py
+++ processor.py
@@ -2,10 +2,74 @@ from PIL import Image
 
 fileName = 'sample/IMG_8218.jpg'
 
-image = Image.open(fileName)
-imageLoad = image.load()
+myImage = Image.open(fileName)
+myImageLoad = myImage.load()
+
+# returns true if all the following are true:
+#	x between values 0 inclusive, horizontal resolution exclusive
+#	y between values 0 inclusive, vertical resolution exclusive
+def imageBoundsCheck(image, x, y):
+	size = image.size
+	return (x >= 0 and y >= 0) and (x < size[0] and y < size[1])
+
+
+def imageBoundsCheckRadius(image, x, y, radius):
+	size = image.size
+	return (x - radius >= 0 and y - radius >= 0) and (x < size[0] - radius and y < size[1] - radius)
+
+
+class RegionChunk:
+	
+	# you give the exact number of Regions in both the vertical and horizontal direction
+	# "posByCorner" = the start X/Y is actually the top left CORNER of a Region instead of the CENTER
+	def chunkDefineExact(self, startX, startY, jumpX, jumpY, regionNumX, regionNumY, regionRadius, posByCorner=False, jumpByRadius=False):
+		navX = startX
+		navY = startY
+		
+		if(jumpByRadius):
+			jumpX = regionRadius
+			jumpY = regionRadius
+		
+		if(posByCorner):
+			navX += regionRadius
+			navY += regionRadius
+		
+		if not imageBoundsCheckRadius(self.imageObject, navX, navY, regionRadius):
+			return False
+		
+		for vert in range(regionNumY):
+			for horz in range(regionNumX):
+				self.regionList.append(Region(self.imageObject, self.imageLoaded, (navX, navY), regionRadius))
+				
+				navX += jumpX
+			
+			navY += jumpY
+			navX -= jumpX * (regionNumX)
+		
+		return True
+		
+	# you give a number of pixels; it will try to pack as many Regions as it can depending on their radius
+	# considered to be more "lazy"
+	def chunkDefinePack(self):
+		pass
+	
+	
+	def __init__(self, imageObject, imageLoaded):
+		self.imageObject = imageObject
+		self.imageLoaded = imageLoaded
+		self.regionList = []
+
+
 
 class Region:
+
+	def imageFillRegion(self, color):
+		centerX = self.pixLocation[0]
+		centerY = self.pixLocation[1]
+		
+		for x in range(centerX - self.pixRadius, centerX + self.pixRadius + 1):
+			for y in range(centerY - self.pixRadius, centerY + self.pixRadius + 1):
+				self.imageLoaded[x, y] = color
 
 	#def getColorDominance(color):
 		
@@ -42,11 +106,40 @@ class Region:
 		return (int(redSum / pixelCount), int(greenSum / pixelCount), int(blueSum / pixelCount))
 
 
-	def __init__(self, imageLoaded, pixLocation, pixRadius=1):
+	def __init__(self, imageObject, imageLoaded, pixLocation, pixRadius=1):
+		self.imageObject = imageObject
 		self.imageLoaded = imageLoaded
-		self.pixLocation = pixLocation
+		self.pixLocation = pixLocation # the center of the region
 		self.pixRadius = pixRadius
 
-r1 = Region(imageLoad, (5, 5), 1)
-print(r1.getRegionPixelList())
-print(r1.getRegionPixelAverage())
+
+
+def regionTestA():	
+	r1 = Region(myImage, myImageLoad, (2, 2), 2)
+
+	print(r1.getRegionPixelList())
+
+	print(r1.getRegionPixelList())
+	print(r1.getRegionPixelAverage())
+
+	r1.imageShowRegion()
+
+
+def chunkTestA():
+	
+	rc1 = RegionChunk(myImage, myImageLoad)
+
+	rc1.chunkDefineExact(0, 0, 0, 0, 40, 20, 50, True, True)
+
+	rc1List = rc1.regionList
+
+	print(len(rc1List))
+
+	for reg in rc1List:
+		#print('REGION: radius = %d, location = %s' % (reg.pixRadius, str(reg.pixLocation)))
+		reg.imageFillRegion(reg.getRegionPixelAverage())
+
+	myImage.show("cooleo")
+
+
+chunkTestA()
